8e10636536615157b9b4baf6391bff1103f5c4c3
whereareiam <mmm.g032@gmail.com>
2024-11-19 17:35:47 +0100

    feat: add predecessor and successor fields to TitleInformation to represent title relationships

--- src/main/java/watch/anikiro/koreki/model/title/TitleInformation.java
+++ src/main/java/watch/anikiro/koreki/model/title/TitleInformation.java
@@ -4,12 +4,28 @@ import lombok.Data;
 import watch.anikiro.koreki.model.Genre;
 import watch.anikiro.koreki.model.Publisher;
 
-import java.util.List;
 import java.util.Set;
 
 @Data
 public class TitleInformation {
     private Set<Genre> genres;
     private Set<Publisher> publishers;
-    private List<Title> relations;
+
+    /*
+     * The predecessor is the title that comes before this one.
+     *
+     * Can be null if there is no predecessor.
+     *
+     * @since 0.0.1
+     */
+    private Long predecessor;
+
+    /*
+     * The successor is the title that comes after this one.
+     *
+     * Can be null if there is no successor.
+     *
+     * @since 0.0.1
+     */
+    private Long successor;
 }
