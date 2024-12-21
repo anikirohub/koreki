2d91d09c60f1b2df8bed264627d2c711260f9ff9
whereareiam <mmm.g032@gmail.com>
2024-12-21 19:58:10 +0100

    refactor: remove predecessor and successor fields from TitleInformation

--- src/main/java/watch/anikiro/koreki/model/title/TitleInformation.java
+++ src/main/java/watch/anikiro/koreki/model/title/TitleInformation.java
@@ -11,22 +11,4 @@ public class TitleInformation {
     private Long id;
     private Set<Genre> genres;
     private Set<Publisher> publishers;
-
-    /*
-     * The predecessor is the title that comes before this one.
-     *
-     * Can be null if there is no predecessor.
-     *
-     * @since 0.0.1
-     */
-    private Long predecessor;
-
-    /*
-     * The successor is the title that comes after this one.
-     *
-     * Can be null if there is no successor.
-     *
-     * @since 0.0.1
-     */
-    private Long successor;
 }
