65da1f85032509983caeca35f635a4a9c99d1f64
whereareiam <mmm.g032@gmail.com>
2025-05-12 13:14:48 +0200

    refactor(auth): change expiresAt field in AuthPairResponse from Instant to long

--- src/main/java/watch/anikiro/koreki/model/auth/pair/AuthPairResponse.java
+++ src/main/java/watch/anikiro/koreki/model/auth/pair/AuthPairResponse.java
@@ -3,12 +3,10 @@ package watch.anikiro.koreki.model.auth.pair;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.time.Instant;
-
 @Data
 @AllArgsConstructor
 public class AuthPairResponse {
 	private String code;
-	private Instant expiresAt;
+	private long expiresAt;
 	private long interval;
 }
