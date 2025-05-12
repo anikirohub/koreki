ac498c6a3e38d9ca40e305b432284a9f72a2bdd0
whereareiam <mmm.g032@gmail.com>
2025-05-12 13:52:10 +0200

    feat(auth): add no-args constructor to AuthResponse class

--- src/main/java/watch/anikiro/koreki/model/auth/AuthResponse.java
+++ src/main/java/watch/anikiro/koreki/model/auth/AuthResponse.java
@@ -2,8 +2,10 @@ package watch.anikiro.koreki.model.auth;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class AuthResponse {
 	private String accessToken;
